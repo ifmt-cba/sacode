06a999a2aea76c6e64fe28c04da2c7090788c7e9
viniciusdemoraess <vinicius.moraes@id-cotton.com>
2024-03-20 09:44:55 -0400

    :sparkles: feat: vo, entitie and services user

---
+++ src/main/java/br/com/edu/ifmt/sacode/domain/services/exception/UserException.java
@@ -0,0 +1,10 @@
+package br.com.edu.ifmt.sacode.domain.services.exception;
+
+public class UserException extends Exception {
+
+    public UserException(String msg) {
+        super(msg);
+    }
+
+}
+

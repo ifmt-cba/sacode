82ce4415e8b27df3e5b3319ab7173e87e89a5fa7
heloisebastos <heloisebastos28@hotmail.com>
2024-03-29 18:37:52 -0400

    :wastebasket: remove: CRUD membroFamiliar

--- src/main/java/br/com/edu/ifmt/sacode/domain/entities/User.java
+++ src/main/java/br/com/edu/ifmt/sacode/domain/entities/User.java
@@ -5,23 +5,22 @@ import java.util.List;
 import java.util.UUID;
 
 import br.com.edu.ifmt.sacode.domain.entities.vo.Email;
+import br.com.edu.ifmt.sacode.domain.entities.vo.Name;
 import br.com.edu.ifmt.sacode.domain.entities.vo.Password;
 import br.com.edu.ifmt.sacode.domain.entities.vo.Username;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class User {
     private UUID idUsuario;
+    private Name name;
     private Username username;
     private Password password;
     private Email email;
     private boolean superUsuario;
-    private List<String> membroFamiliar;
+    private List<Name> membroFamiliar;
 
     public User() {
         this.idUsuario = new UUID(0, 0);
+        this.name = new Name(null);
         this.username = new Username(null);
         this.password = new Password(null);
         this.email = new Email(null);
@@ -69,46 +68,20 @@ public class User {
         this.superUsuario = superUsuario;
     }
 
-    public List<String> getMembroFamiliar() {
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public List<Name> getMembroFamiliar() {
         return membroFamiliar;
     }
 
-    public void setMembroFamiliar(List<String> membroFamiliar) {
+    public void setMembroFamiliar(List<Name> membroFamiliar) {
         this.membroFamiliar = membroFamiliar;
-    }
-
-    public void addMembroFamiliar(String membro) {
-        if (membroFamiliar == null) {
-            membroFamiliar = new ArrayList<>();
-        }
-        String newMembro = membro.toLowerCase();
-        membroFamiliar.add(newMembro);
-    }
-
-    public void removeMembroFamiliar(String membro) {
-        if (membroFamiliar != null) {
-            for (int i = 0; i < membroFamiliar.size(); i++) {
-                if (membroFamiliar.get(i).toLowerCase().equals(membro)) {
-                    membroFamiliar.remove(i);
-                    break;
-                }
-            }
-        }
-    }
-
-    public void alterMembroFamiliar(String membro, String newMembro) {
-        if (membroFamiliar != null) {
-            int aux = -1;
-            for (int i = 0; i < membroFamiliar.size(); i++) {
-                if (membroFamiliar.get(i).toLowerCase().equals(newMembro)) {
-                    aux = i;
-                    break;
-                }
-            }
-            if (aux != -1) {
-                membroFamiliar.set(aux, newMembro);
-            }
-        }
     }
 
     @Override
@@ -116,6 +89,7 @@ public class User {
         return String.format("""
                 {
                     "idUsuario":"%s",
+                    "name":"%s",
                     "username":"%s",
                     "email":"%s",
                     "superUsuario":"%s",
@@ -123,6 +97,7 @@ public class User {
                 }
                 """,
                 idUsuario.toString(),
+                name.toString(),
                 username.toString(),
                 email.toString(),
                 Boolean.toString(superUsuario),

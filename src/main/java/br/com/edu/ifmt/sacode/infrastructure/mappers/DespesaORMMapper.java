798960e44c32cc8d07bf2861364d8368ea20c469
Wenderson Farias <wendersonpfarias@gmail.com>
2024-07-27 13:17:37 -0400

    :recycle: refactor: Alteração dos métodos para estáticos e adição do setter de usuário no DespesaORMMapper

--- src/main/java/br/com/edu/ifmt/sacode/infrastructure/mappers/DespesaORMMapper.java
+++ src/main/java/br/com/edu/ifmt/sacode/infrastructure/mappers/DespesaORMMapper.java
@@ -1,46 +1,55 @@
 package br.com.edu.ifmt.sacode.infrastructure.mappers;
 
+import br.com.edu.ifmt.sacode.domain.entities.Despesa;
+import br.com.edu.ifmt.sacode.domain.entities.vo.Descricao;
+import br.com.edu.ifmt.sacode.domain.entities.vo.Moeda;
+import br.com.edu.ifmt.sacode.domain.entities.vo.Nome;
+import br.com.edu.ifmt.sacode.infrastructure.persistence.DespesaORM;
+import br.com.edu.ifmt.sacode.infrastructure.persistence.UsuarioORM;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.stereotype.Component;
-
-import br.com.edu.ifmt.sacode.domain.entities.Despesa;
-import br.com.edu.ifmt.sacode.domain.entities.vo.Nome;
-import br.com.edu.ifmt.sacode.domain.entities.vo.Descricao;
-import br.com.edu.ifmt.sacode.domain.entities.vo.Moeda;
-import br.com.edu.ifmt.sacode.infrastructure.persistence.DespesaORM;
-
 @Component
 public class DespesaORMMapper {
-  public DespesaORM toORM(Despesa despesaDomainObj) {
+    CategoriaORMMapper categoriaORMMapper;
+
+  public static DespesaORM toORM(Despesa despesaDomainObj) {
+    UsuarioORM usuario = new UsuarioORM();
+    usuario.setIdUsuario(despesaDomainObj.getUsuario().toString());
+
     return new DespesaORM(
             despesaDomainObj.getIdDespesa(),
             despesaDomainObj.getDescricao(),
             despesaDomainObj.getValor(),
             despesaDomainObj.getData(),
-            despesaDomainObj.getUsuario(),
+            usuario,
             despesaDomainObj.getAutorDespesa(),
             despesaDomainObj.isFixa(),
             despesaDomainObj.getFinanciadorDespesa(),
             despesaDomainObj.getCorrelacaoParcelas(),
-            despesaDomainObj.getNumParcela()
+            despesaDomainObj.getNumParcela(),
+            CategoriaORMMapper.dominioParaOrm(despesaDomainObj.getCategoria())
         );
   }
 
-  public Despesa toDomainObj(DespesaORM despesaORM) {
+  public static Despesa toDomainObj(DespesaORM despesaORM) {
+    CategoriaORMMapper categoriaORMMapper = new CategoriaORMMapper();
+
     Despesa despesa = new Despesa();
     despesa.setIdDespesa( UUID.fromString( despesaORM.getIdDespesa()));
     despesa.setDescricao(new Descricao(despesaORM.getDescricao().toString()));
     despesa.setValor( new Moeda(despesaORM.getValor()));
     despesa.setData( despesaORM.getData());
-    despesa.setUsuario( UUID.fromString(despesaORM.getUsuario()));
+    despesa.setUsuario(UUID.fromString(despesaORM.getUsuario().getIdUsuario()));
     despesa.setAutorDespesa(new Nome(despesaORM.getAutorDespesa().toString()));
     despesa.setFixa( despesaORM.getFixa());
     despesa.setFinanciadorDespesa(new Nome(despesaORM.getFinanciadorDespesa().toString()));
     despesa.setCorrelacaoParcelas(  UUID.fromString(despesaORM.getCorrelacaoParcelas()));
     despesa.setNumParcela(despesaORM.getNumParcela());
+    despesa.setCategoria(CategoriaORMMapper.ormParaDominio(despesaORM.getCategoria()));
     return despesa;
   }
   public List<Despesa> toDomainList(List<DespesaORM> despesasORM) {

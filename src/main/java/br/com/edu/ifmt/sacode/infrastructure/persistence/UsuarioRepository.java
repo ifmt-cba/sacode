5840414a50277177065dd899d9b92219a2656c6c
heloisebastos <heloisebastos28@hotmail.com>
2024-07-28 22:44:53 -0400

    :recycle: refactor: Altera para o tipo String - UsuarioRepository

--- src/main/java/br/com/edu/ifmt/sacode/infrastructure/persistence/UsuarioRepository.java
+++ src/main/java/br/com/edu/ifmt/sacode/infrastructure/persistence/UsuarioRepository.java
@@ -1,8 +1,9 @@
 package br.com.edu.ifmt.sacode.infrastructure.persistence;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends CrudRepository<UsuarioORM, Long> {
+public interface UsuarioRepository extends CrudRepository<UsuarioORM, String> {
     UsuarioORM findByIdUsuario(String id);
 }

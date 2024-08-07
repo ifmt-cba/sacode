cc6fd80f52ead71cadb89cfc1b257f58ab2070a8
Tiramt <83783369+Thiagoramalhosetubal@users.noreply.github.com>
2024-08-07 09:20:37 -0300

    :tada: Adicionando swagger

---
+++ src/main/java/br/com/edu/ifmt/sacode/Controller/SwaggerController.java
@@ -0,0 +1,18 @@
+package br.com.edu.ifmt.sacode.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/helloworld")
+public class SwaggerController {
+
+    @Operation(summary = "Hello", description = "Retorna uma mensagem de saudação", tags = "Helloworld")
+     @GetMapping
+    public String helloWorld() {
+        return "helloworld";
+    }
+}

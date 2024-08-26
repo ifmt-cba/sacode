a9a083b931d5173f49c757d4d3ea26c49a6dcda6
viniciusdemoraess <vinicius.moraes@id-cotton.com>
2024-08-26 11:02:45 -0400

    :bug: fix: ajuste errors in service e repository

--- src/main/java/br/com/edu/ifmt/sacode/domain/entities/vo/Nome.java
+++ src/main/java/br/com/edu/ifmt/sacode/domain/entities/vo/Nome.java
@@ -4,16 +4,16 @@ import java.util.regex.Pattern;
 
 /**
  * Um nome válido deve atender a seguinte regex :
- * ^[a-zA-ZÀ-ÿ\\s'-]{8,80}$
+ * ^[a-zA-ZÀ-ÿ\\s'-]{2,79}$
  * ^ Tipifica que a string deve corresponder a regex do inicio ao final
  * [a-zA-ZÀ-ÿ\\s'-] Um conjunto de caracteres que inclui letras de A a Z
  * (maiúsculas e minúsculas),
  * letras acentuadas (como À-ÿ), espaços, hifens e apóstrofos.
- * {8,80} restrição de comprimento
+ * {2,79} restrição de comprimento
  * $ Fim da string
  */
 public class Nome {
-    private static final String regexNome = "^[a-zA-ZÀ-ÿ\\s'-]{7,79}$";
+    private static final String regexNome = "^[a-zA-ZÀ-ÿ\\s'-]{2,79}$";
 
     private String nome;
 

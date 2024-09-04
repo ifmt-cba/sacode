5d7d9bb591be89fa997828054e7c7dad26dafc73
viniciusdemoraess <vinicius.moraes@id-cotton.com>
2024-09-04 00:15:39 -0400

    :broom: cleanup: retirado sysout desnecessários

--- src/main/java/br/com/edu/ifmt/sacode/SacodeApplication.java
+++ src/main/java/br/com/edu/ifmt/sacode/SacodeApplication.java
@@ -17,20 +17,4 @@ public class SacodeApplication {
 		SpringApplication.run(SacodeApplication.class, args);
 	}
 
-	// CLI para iniciar o web-service: ./mvnw spring-boot:run
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}
-
 }
